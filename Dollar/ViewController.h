d56e3ef1b9a008b01ccffc7b9cf20527ca59fb4c
samoht9277 <totobighouse@gmail.com>
2021-11-30 21:16:01 -0300

    Cleaned code base. Added update button and improved splash screen.

--- Dollar/ViewController.h
+++ Dollar/ViewController.h
@@ -7,13 +7,17 @@
 
 #import <UIKit/UIKit.h>
 
+@interface ViewController : UIViewController
+@end
+
 @interface Dollar : NSObject
--(NSArray *) getDataFrom:(NSString *)link type:(NSString *)type;
+- (NSArray *) getDataFrom:(NSString *)link type:(NSString *)type;
+- (void) updateLabel:(UILabel *)str1 str2:(UILabel *)str2 str3:(UILabel *) str3 str4:(UILabel *)str4;
 @end
 
 @implementation Dollar
 
--(NSArray *) getDataFrom:(NSString *)link type:(NSString *)type {
+- (NSArray *) getDataFrom:(NSString *)link type:(NSString *)type {
     NSString *url = [link stringByAppendingString:type];
     
     NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
@@ -30,10 +34,15 @@
     return 0;
 }
 
+//    [dollar updateLabel:_officialVenta str2:_officialCompra str3:_blueCompra str4:_blueVenta];
+- (void) updateLabel:(UILabel *)str1 str2:(UILabel *)str2 str3:(UILabel *) str3 str4:(UILabel *)str4 {
+    NSArray *blue = [self getDataFrom:@"https://dollar-api.herokuapp.com/" type:@"blue"];
+    NSArray *official = [self getDataFrom:@"https://dollar-api.herokuapp.com/" type:@"official"];
+    
+    str1.text = [NSString stringWithFormat:@"$%@", official[0]];
+    str2.text = [NSString stringWithFormat:@"$%@", official[1]];
+    str3.text = [NSString stringWithFormat:@"$%@", blue[0]];
+    str4.text = [NSString stringWithFormat:@"$%@", blue[1]];
+}
+
 @end
-
-@interface ViewController : UIViewController
-
-
-@end
-
--- Dollar/ViewController.m
+++ Dollar/ViewController.m
@@ -6,8 +6,13 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioServices.h>
 
 @interface ViewController ()
+- (IBAction)dataProvider:(id)sender;
+- (IBAction)updateButton:(id)sender;
+- (void)update;
+@property uint8_t tries;
 @property (weak, nonatomic) IBOutlet UILabel *blueVenta;
 @property (weak, nonatomic) IBOutlet UILabel *blueCompra;
 @property (weak, nonatomic) IBOutlet UILabel *officialVenta;
@@ -17,8 +22,14 @@
 
 @implementation ViewController
 
+- (void)update {
+    id dollar = [[Dollar alloc] init];
+    [dollar updateLabel:_officialCompra str2:_officialVenta str3:_blueCompra str4:_blueVenta];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     //GRADIENT
     CAGradientLayer *gradient = [CAGradientLayer layer];
     gradient.frame = _BG.bounds;
@@ -26,21 +37,23 @@
     gradient.endPoint = CGPointMake(1, 1);
     gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithRed:225.0/255.0 green:108.0/255.0 blue:171.0/255.0 alpha:1.0] CGColor], (id)[[UIColor colorWithRed:115/255.0 green:102/255.0 blue:255/255.0 alpha:1.0] CGColor], nil];
     [_BG.layer insertSublayer:gradient atIndex:0];
+      
+    _tries = 0;
     
     // DOLLAR
-    id dollar = [[Dollar alloc] init];
-    NSArray *blue = [dollar getDataFrom:@"https://dollar-api.herokuapp.com/" type:@"blue"];
-    NSArray *official = [dollar getDataFrom:@"https://dollar-api.herokuapp.com/" type:@"official"];
-    
-    _officialVenta.text = [NSString stringWithFormat:@"$%@", official[0]];
-    _officialCompra.text = [NSString stringWithFormat:@"$%@", official[1]];
-    _blueCompra.text = [NSString stringWithFormat:@"$%@.00", blue[0]];
-    _blueVenta.text = [NSString stringWithFormat:@"$%@.00", blue[1]];
-    
-    //NSLog(@"BLUE (compra, venta): %@ %@", blue[0], blue[1]);
-    //NSLog(@"OFFICIAL (compra, venta): %@ %@", official[0], official[1]);
+    [self update];
+}
+
+- (IBAction)updateButton:(id)sender {
+    AudioServicesPlaySystemSound(1519);
+    if (_tries == 5) return; 
+    [self update];
+    _tries++;
+}
+
+- (IBAction)dataProvider:(id)sender {
+    AudioServicesPlaySystemSound(1519);
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://dolarhoy.com"] options:@{} completionHandler:nil];
 }
 
 @end
-
-
